6bbc8dbf6a2b9df4987ea1e89604c583ae7ded0a
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2017-11-26 16:04:19 -0700

    hw3:aded q1 tag counter

--- statNlp_Mihai/hw3/q1/initializer.py
+++ statNlp_Mihai/hw3/q1/initializer.py
@@ -2,7 +2,7 @@ from utils.readData import read_without_space
 
 import os
 import time
-
+from tqdm import tqdm
 start_time = time.time()
 
 trainingData="train.tagged"
@@ -12,8 +12,19 @@ testingData="test.tagged"
 cwd = os.getcwd()
 posTrain=read_without_space(cwd,trainingData)
 
-for word in posTrain["words"]:
-    print(word)
+# for word in posTrain["words"]:
+#     print(word)
+
+
+tagCounter={}
+for tag in tqdm(posTrain["tags"],total=len(posTrain["tags"])):
+    if tag in tagCounter:
+        tagCounter[tag] += 1
+    else:
+        tagCounter[tag] = 1
+
+wordTagCounter={}
+
 
 #prepare_training_data(posTrain)
 
