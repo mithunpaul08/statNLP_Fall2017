dabe598b82fbe1f42b4031c021e382a58c81365b
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2017-11-25 20:26:44 -0700

    hw3:added execution time

--- statNlp_Mihai/hw3/hw3_grad_mithun_paul/initializer.py
+++ statNlp_Mihai/hw3/hw3_grad_mithun_paul/initializer.py
@@ -2,6 +2,10 @@ from utils.readData import readSpam
 from utils.lstm import startLstm
 import os
 
+import time
+
+start_time = time.time()
+
 trainingData="train.tagged"
 devData="dev.tagged"
 testingData="test.tagged"
@@ -9,3 +13,4 @@ testingData="test.tagged"
 cwd = os.getcwd()
 posTrain=readSpam(cwd,trainingData)
 startLstm(posTrain)
+print("--- %s seconds ---" % (time.time() - start_time))
